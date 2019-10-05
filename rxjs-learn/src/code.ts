af2c4314f6881a84cb96ad9fd4c019aa0298c2a5
mkozi <mkozi.gh01@gmail.com>
2019-10-05 08:59:44 -0500

    coding up to unsubscribe

---
+++ rxjs-learn/src/code.ts
@@ -0,0 +1,41 @@
+import { Observable } from 'rxjs/Observable';
+
+function addItem(val: any) {
+    const node = document.createElement('li');
+    const textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
+
+// const observable1 = Observable.create(function subscribe(observer: any) {
+//     observer.next('Hey there')
+// });
+
+// Creating an observable
+const observable2 = Observable.create((observer: any) => {
+    try {
+        observer.next('Value 1');
+        // throw new Error('oh no!');
+        observer.next('Value 2');
+
+        setInterval(() => {
+            observer.next('more values');
+        }, 2000);
+
+        // observer.complete();
+        observer.next('Value 3');  // not sent, because 'next' after the complete has been called on the observer
+    } catch(err) {
+        observer.error(err);
+    }
+});
+
+// Subscribing to an observable
+var observer = observable2.subscribe(
+    (x: any) => { addItem(x); },    // the next handler
+    (error: any) => { addItem(`There was an error:: ${error}`); },              // the error handler
+    () => { addItem('Completed') }  // the completed handler
+)
+
+setTimeout(() => {
+    observer.unsubscribe();
+}, 6001);
