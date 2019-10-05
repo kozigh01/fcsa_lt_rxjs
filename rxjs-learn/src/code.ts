2e4ec8e83795bbee8412f780b0b5b1de8940dbff
Mkozi <mkozi.junk@gmail.com>
2019-10-05 14:08:58 -0500

    Async subject

--- rxjs-learn/src/code.ts
+++ rxjs-learn/src/code.ts
@@ -1,4 +1,4 @@
-import { ReplaySubject } from 'rxjs';
+import { AsyncSubject } from 'rxjs';
 
 function addItem(val: any, outputArea: string = "output1") {
     const node = document.createElement('li');
@@ -8,26 +8,21 @@ function addItem(val: any, outputArea: string = "output1") {
 }
 
 // subject is an observer that can also function as an observable (so, can emit values)
-const subject = new ReplaySubject(3);
+const subject = new AsyncSubject();
 
-subject.subscribe(
+const observer1 = subject.subscribe(
     data => addItem(`Observer 1: ${data}`),
     err => addItem(`Observer 1 Error:: ${err}`),
     () => addItem(`Observer 1 completed`)
 );
 
-subject.next('Value 1');
-subject.next('Value 2');
-subject.next('Value 3');
-subject.next('...Observer 2 is about to subscribe...');
+let count = 1;
+const int = setInterval(() => subject.next(count++), 100);
 
-var observer2 = subject.subscribe(
-    data => addItem(`Observer 2:: ${data}`, 'output2'),
-    err => addItem(`Observer 2 Error:: ${err}`, 'output2'),
-    () => addItem(`Observer 2 completed`, 'output2')
-)
+setTimeout(() => {
+    const observer2 = subject.subscribe(
+        data => addItem(`Observer 2:: ${data}`, 'output2')
+    );
+    subject.complete();
+}, 800);
 
-subject.next('Value 4');
-
-subject.complete();
-subject.next('Value 5');
