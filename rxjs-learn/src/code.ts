758e1c0ab3f5acbaa32056e0988f1be26f6cb412
Mkozi <mkozi.junk@gmail.com>
2019-10-06 07:42:27 -0500

    add map example

--- rxjs-learn/src/code.ts
+++ rxjs-learn/src/code.ts
@@ -9,3 +9,16 @@ merge(
     of('World').pipe( delay(2000) ),
     of('!!').pipe( delay(4000))
 );
+
+// map example
+const { of, merge } = Rx;
+const { delay, map } = RxOperators;
+ 
+merge(
+    of('Hello'),
+    of('World').pipe( delay(2000) ),
+    of('!!').pipe( delay(4000))
+)
+.pipe(
+	map(x => x.toUpperCase())
+);
